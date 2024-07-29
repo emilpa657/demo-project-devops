c65da8303a7014ab4cdc022817244ad1eb22cbfc
unknown <emilpa657@gmail.com>
2024-07-29 11:47:23 -0400

    initial commit

---
+++ src/test/java/com/edurekadevops/demo_project_devops/DemoProjectDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edurekadevops.demo_project_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
